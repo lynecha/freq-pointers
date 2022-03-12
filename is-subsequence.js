e2958c02a70c9dd488c7e91c573188060d34001e
lyne cha <lynecha@berkeley.edu>
2022-03-11 17:13:58 -0800

    finished up to subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,23 @@
-// add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+"use strict";
+
+/** takes two strings checks if the first string is a subsequence of the second string and order matters
+ *  sing sting
+ *   i    l
+ */
+function isSubsequence(str1,str2) {
+    let str1LeftPointer = 0;
+    let str2LeftPointer = 0;
+    let counter = 0;
+
+    while (str1LeftPointer < str1.length && str2LeftPointer <str2.length) {
+        if (str1[str1LeftPointer] === str2[str2LeftPointer]) {
+            str1LeftPointer++;
+            str2LeftPointer++;
+            counter++;
+        }
+        else {
+            str2LeftPointer++;
+        }
+    }
+    return counter === str1.length;
 }
