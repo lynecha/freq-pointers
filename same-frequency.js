8a7ea7c6af7afa77cf98d4b8c2149a830fb92054
Michael Paglione <pagman777@gmail.com>
2022-03-11 19:56:28 -0500

    solve same-frequency, solve seperate-positive" "

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,35 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict";
+
+/**Convert numbers to strings and feed into frequency counter
+ * Compare string lengths and frequences*/
+function sameFrequency(num1, num2) {
+
+  const numString1 = createFrequencyCounter(num1.toString());
+  const numString2 = createFrequencyCounter(num2.toString());
+
+  if (numString1.length !== numString2.length) {
+    return false;
+  }
+  for (let key of numString1.keys()) {
+
+    if (numString1.get(key) !== numString2.get(key)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/** Frequency counter to sort strings and arrays */
+function createFrequencyCounter(word) {
+  let frequencyCounter = new Map();
+
+  for (let char of word) {
+    if (!frequencyCounter.has(char)) {
+      frequencyCounter.set(char, 1);
+    }
+    else {
+      frequencyCounter.set(char, frequencyCounter.get(char) + 1);
+    }
+  }
+  return frequencyCounter;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,33 @@
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+
+/* left and right pointers
+while left is smaller than right
+left is negative? swap positions increment left
+right is positive swap postions decrement right
+
+*/
+/** take in array of nums and sort negative numbers on right
+ * array and positive on left of array */
+function separatePositive(nums) {
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right){
+    if (nums[left] < 0){
+      if(nums[right] > 0){
+        let temp = nums[left];
+
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++
+        right--
+      }else {
+        right--
+      }
+    }else {
+      left++
+    }
+  }
+  return nums
 }
